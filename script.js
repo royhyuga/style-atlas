6b7886c7e28a7ee434ebca72766b52f3ee3c8a59
royhyuga <jp1234540@gmail.com>
2025-05-18 22:17:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Cargar artículos
-  fetch("articles/articles.json")
-    .then(response => response.json())
-    .then(data => {
-      const container = document.getElementById("articles-container");
-      const latest = data.slice(0, 2);
-      latest.forEach(article => {
-        const card = document.createElement("a");
-        card.href = article.link;
-        card.className = "card";
-        card.innerHTML = `
-          <img src="${article.img}" alt="${article.title}">
-          <h3>${article.title}</h3>
-          <p>${article.summary}</p>
-        `;
-        container.appendChild(card);
-      });
+fetch("articles/articles.json")
+  .then(response => response.json())
+  .then(data => {
+    const latest = data.reverse().slice(0, 2);
+    const container = document.getElementById("articles-container");
+    latest.forEach(article => {
+      const card = document.createElement("a");
+      card.href = article.link;
+      card.className = "card";
+      card.innerHTML = `
+        <img src="${article.img}" alt="${article.title}">
+        <h3>${article.title}</h3>
+        <p>${article.summary}</p>
+      `;
+      container.appendChild(card);
     });
+  });
 
   // Cargar noticias
   fetch("news/news.json")
-    .then(response => response.json())
-    .then(data => {
-      const container = document.getElementById("news-container");
-      const latest = data.slice(0, 1);
-      latest.forEach(news => {
-        const card = document.createElement("a");
-        card.href = news.link;
-        card.className = "card";
-        card.innerHTML = `
-          <img src="${news.img}" alt="${news.title}">
-          <h3>${news.title}</h3>
-          <p>${news.summary}</p>
-        `;
-        container.appendChild(card);
-      });
+  .then(response => response.json())
+  .then(data => {
+    const latest = data.reverse().slice(0, 1);
+    const container = document.getElementById("news-container");
+    latest.forEach(news => {
+      const card = document.createElement("a");
+      card.href = news.link;
+      card.className = "card";
+      card.innerHTML = `
+        <img src="${news.img}" alt="${news.title}">
+        <h3>${news.title}</h3>
+        <p>${news.summary}</p>
+      `;
+      container.appendChild(card);
     });
+  });
 
   // Menú hamburguesa
   const menuBtn = document.getElementById("menu-toggle");
